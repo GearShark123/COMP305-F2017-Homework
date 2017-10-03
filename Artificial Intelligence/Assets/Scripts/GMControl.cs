f42ac2a3071aac992f57cfe555243c1ad040a0f8
GearShark123 <lylespurrell@hotmail.com>
2017-10-03 16:48:56 -0400

    [ADD] GUI Buttons Rough Draft

--- Artificial Intelligence/Assets/Scripts/GMControl.cs
+++ Artificial Intelligence/Assets/Scripts/GMControl.cs
@@ -11,25 +11,49 @@ public class GMControl : MonoBehaviour
     public float screenHeight;
 
     public GameObject checkpointMarker;
+    public Transform ship;
+    private float counter;
+    public bool isNotBtn;
+    private List<Transform> checkpointList = new List<Transform>();
 
-    // Use this for initialization
     void Start()
     {
-
+        isNotBtn = true;       
     }
 
     // Update is called once per frame
     void Update()
-    {
-        mousePosition = Input.mousePosition;
-        screenToWorldPoint_Position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
-        screenWidth = Screen.width;
-        screenHeight = Screen.height;
+    {    
+        // Testing
+        //mousePosition = Input.mousePosition;
+        //screenToWorldPoint_Position = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+        //screenWidth = Screen.width;
+        //screenHeight = Screen.height;
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && counter <= 9 && isNotBtn == true)
         {
-            Instantiate(checkpointMarker, Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, this.transform.position.z + 10f)), this.transform.rotation);
+            counter += 1;
+            GameObject nextcheckpointMarker = Instantiate(checkpointMarker, Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, this.transform.position.z + 10f)), this.transform.rotation);
+            nextcheckpointMarker.name = "Checkpoint Marker " + counter;
+            checkpointList.Add(nextcheckpointMarker.transform);
+        }        
+    }
+
+    public void DisableCheckpointDrop()
+    {
+        isNotBtn = false;
+    }
+    public void EnableCheckpointDrop()
+    {
+        isNotBtn = true;
+    }
+
+    public void Begin()
+    {
+        counter = 0;
+        foreach (var item in checkpointList)
+        {
+            ship.position = Vector3.MoveTowards(ship.position, item.position, 3 * Time.deltaTime);
         }
     }
 }
